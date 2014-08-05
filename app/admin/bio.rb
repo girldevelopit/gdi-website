4ff1fb9bc39b3c277960f4fe18b07ff9eb6fba08
Ruthan <rcfreese@gmail.com>
2014-08-05 11:29:29 -0400

    Allow more attributes

--- app/admin/bio.rb
+++ app/admin/bio.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Bio do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :bio
+  permit_params :bio, :user, :title, :name, :info, :adminuser
   #
   # or
   #
