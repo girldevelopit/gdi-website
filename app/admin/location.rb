bcd717b153ad1e9f61fc02b05987bc7329e37ba7
Ruthan <rcfreese@gmail.com>
2014-08-05 11:01:22 -0400

    Update admin controllers to permit attributes

--- app/admin/location.rb
+++ app/admin/location.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Location do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :location
   #
   # or
   #
