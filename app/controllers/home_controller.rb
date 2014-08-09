8d45cedbe0bda2bba27878db185b91987c9d0c2e
Ruthan <rcfreese@gmail.com>
2014-08-09 12:57:39 -0400

    Begin Meetup integration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,17 @@
 class HomeController < ApplicationController
   def index
     locational # in applaction controller
+    params = { category: '1',
+          zip: '27701',
+          # city: 'Raleigh',
+          # state: 'NC',
+          # country: 'USA',
+          status: 'upcoming',
+          format: 'json',
+          page: '5'}
+
+    meetup_api = MeetupApi.new
+    @events = meetup_api.open_events(params)
   end
 
   def about
