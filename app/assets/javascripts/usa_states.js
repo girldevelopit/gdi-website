359148cec98223c3ff7a0a4a2f65f8d07e8a5278
amygori <amydgori@gmail.com>
2014-08-10 13:48:16 -0400

    Fixed compilation errors for map js files; added scripts to initializers/assets.rb

--- app/assets/javascripts/usa_states.js
+++ app/assets/javascripts/usa_states.js
@@ -45,7 +45,7 @@
 							scaleY= -1;
 
 						} else if (lon < -140) { // hawaii
-							// Lat: 18°?55' N to 28°?27' N, Lng:154°?48' W to 178°?22' W
+							// Lat: 18Â°?55' N to 28Â°?27' N, Lng:154Â°?48' W to 178Â°?22' W
 							// (225, 504) to (356, 588) on map
 
 							// these are guesses
