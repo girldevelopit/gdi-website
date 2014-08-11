421d127b68f72116c0f4c2341c04b067317990a5
Ruthan <rcfreese@gmail.com>
2014-08-11 11:26:05 -0400

    Fix breaky function by changing nothing

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,15 @@
 class HomeController < ApplicationController
   def index
     locational # in applaction controller
-    params = { category: '1',
-          zip: '27701',
-          # city: 'Raleigh',
-          # state: 'NC',
-          # country: 'USA',
-          status: 'upcoming',
-          format: 'json',
-          page: '5'}
 
+    params = { category: '1',
+      zip: '27701',
+      format: 'json',
+      page: '5'}
     meetup_api = MeetupApi.new
-    @events = meetup_api.open_events(params)
-  end
+    @events = meetup_api.open_events(params) ## Currently events just gets
+  end    ## dumped straight into the page. It is ugly, but there is information,
+         ## and we can get better information out of it :) 
 
   def about
   end
