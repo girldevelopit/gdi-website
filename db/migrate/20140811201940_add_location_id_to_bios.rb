3679a730d24db0c1cadc040436a25b74d35fae3e
Ruthan <rcfreese@gmail.com>
2014-08-12 16:48:18 -0400

    Add leader bios to seed data

--- db/migrate/20140811201940_add_location_id_to_bios.rb
+++ db/migrate/20140811201940_add_location_id_to_bios.rb
@@ -1,5 +1,6 @@
 class AddLocationIdToBios < ActiveRecord::Migration
   def change
-    add_column :bios, :location_id, :string
+    add_column :bios, :location_id, :integer
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140812143835) do
+ActiveRecord::Schema.define(version: 20140811201940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 20140812143835) do
     t.datetime "updated_at"
     t.integer  "admin_user_id"
     t.string   "image"
-    t.string   "location_id"
+    t.integer  "location_id"
   end
 
   create_table "locations", force: true do |t|
