754a30318a51e97bd16eceb8ab8497916320f13f
amygori <amydgori@gmail.com>
2014-08-13 11:51:16 -0400

    Added the templates via script tags

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,3 @@
 //= require turbolinks
 //= require_tree .
 
-var api_key = "315d52644b474a1e61687d5bb337f62"
-var chapter_id = 
-var meetup_url = '"https://api.meetup.com/2/events?key=" + api_key + "&sign=true&group_id=" + chapter_id '
-
-$(document).ready(function(){
-	$.getJSON(meetup_url).done(function(classes){
-		$.each
-	})
-}
