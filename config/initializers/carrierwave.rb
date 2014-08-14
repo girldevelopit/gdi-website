1420d31815c409fc0e9045fd5c10344b4f73d87d
Chet Corey <chetcorey@hotmail.com>
2014-08-14 14:04:00 -0400

    just kidding still not working

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,18 +2,16 @@ CarrierWave.configure do |config|
   config.permissions = 0666
   config.directory_permissions = 0777
 
-config.storage = :file
+  if Rails.env.production?
+    config.storage = :fog
 
-  # if Rails.env.production?
-  #   config.storage = :fog
-  #
-  #   config.fog_credentials = {
-  #       :provider               => 'AWS',
-  #       :aws_access_key_id      => Rails.application.secrets.aws_access_key,
-  #       :aws_secret_access_key  => Rails.application.secrets.aws_secret_key
-  #   }
-  #   config.fog_directory = 'gdi'
-  # else
-  #   config.storage = :file
-  # end
+    config.fog_credentials = {
+        :provider               => 'AWS',
+        :aws_access_key_id      => Rails.application.secrets.aws_access_key,
+        :aws_secret_access_key  => Rails.application.secrets.aws_secret_key
+    }
+    config.fog_directory = 'gdi'
+  else
+    config.storage = :file
+  end
 end
