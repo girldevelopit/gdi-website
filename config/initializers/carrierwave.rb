4dc18cf2b6a3d4dc1e29350738709f0e948bcfc4
Chet Corey <chetcorey@hotmail.com>
2014-08-15 09:36:23 -0400

    Attempting to get S3 working

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,15 +2,15 @@ CarrierWave.configure do |config|
   config.permissions = 0666
   config.directory_permissions = 0777
 
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => Rails.application.secrets.aws_access_key,
+      :aws_secret_access_key  => Rails.application.secrets.aws_secret_key
+  }
+
   if Rails.env.production?
     config.storage = :fog
-
-    config.fog_credentials = {
-        :provider               => 'AWS',
-        :aws_access_key_id      => Rails.application.secrets.aws_access_key,
-        :aws_secret_access_key  => Rails.application.secrets.aws_secret_key
-    }
-    config.fog_directory = 'gdi'
+    config.fog_directory = 'girl_develop_it'
   else
     config.storage = :file
   end
