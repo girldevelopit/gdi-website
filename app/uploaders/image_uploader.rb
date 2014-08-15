5abe269aa3580a942bb9fe0923ac0b31cb40e8d1
Chet Corey <chetcorey@hotmail.com>
2014-08-15 17:55:42 -0400

    new sized imaged and maps

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process :store_dimensions
+  # process :store_dimensions
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
@@ -13,8 +13,6 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   process :resize_to_fit => [300, nil]
   process :crop => '300x300+0+0'
-
-binding.pry
 
   version :thumb do
     process :resize_to_fit => [100, 150]
@@ -39,11 +37,11 @@ binding.pry
 
   private
 
-  def store_dimensions
-    if file && model
-      model.width, model.height = ::MiniMagick::Image.open(file.file)[:dimensions]
-    end
-  end
+  # def store_dimensions
+  #   if file && model
+  #     model.width, model.height = ::MiniMagick::Image.open(file.file)[:dimensions]
+  #   end
+  # end
 
   def crop(geometry)
   manipulate! do |img|
