6e4fcefa69bf2656a72c780c58b18d2e591600fe
amygori <amydgori@gmail.com>
2014-08-16 16:25:33 -0400

    ajax request success but variables still not right

--- app/assets/javascripts/chapter_map.js
+++ app/assets/javascripts/chapter_map.js
@@ -1,60 +1,60 @@
 var locationsURL = "http://localhost:3000/locations.json"
 
 $(document).ready(function(){
-	var newPlots = {
-		"<%= @location.location %>" : {
-			latitude: "<%= @location.latitude %>",
-			longitude: "<%= @location.longitude %>",
-			//href: "locations/<% slug %>",
-			//tooltip: {content : "<% location %>"}
-		}
-	};
+	
 	$.getJSON(locationsURL).done(function (chapter) { 
 		console.log(chapter);
 		_.each(chapter, function (plot) {
 			console.log(plot);
-			//feed coordinates into the variable above
-
+			$(".us_map").mapael({
+				map : {
+					name : "usa_states",
+					cssClass : "map",
+					tooltip : {
+						cssClass : "mapTooltip" //class name of the tooltip container
+						},
+					defaultArea : {
+						attrs : {
+							fill : "#282828",
+							stroke: "#9a9a9a",
+						},
+						attrsHover : {
+							fill : "#d8ae64"
+						}
+					},
+					defaultPlot : {
+						type : "circle",
+						href : "#",
+						attrs: {
+							fill : "#f95a61"
+						}
+					},
+					eventHandlers: {
+						click : function (){
+							this.href="locations/<%= slug %>"  
+						}
+					}
+				},
+				plots: {
+					'nc' : {
+						latitude: 35.7806,
+						longitude: -78.6389,
+						tooltip: {content : "Raleigh-Durham"}
+					},	
+				}
+			});
 		});
+		var newPlots = {
+			"<%= location %>" : {
+			latitude: "<%= latitude %>",
+			longitude: "<%= longitude %>",
+			//href: "locations/<% slug %>",
+			//tooltip: {content : "<% location %>"}
+			}
+		};
 	$(".us_map").trigger("update", newPlots);
 	});
 
- 	$(".us_map").mapael({
-		map : {
-			name : "usa_states",
-			cssClass : "map",
-			tooltip : {
-				cssClass : "mapTooltip" //class name of the tooltip container
-			},
-			defaultArea : {
-				attrs : {
-					fill : "#282828",
-					stroke: "#9a9a9a",
-				},
-				attrsHover : {
-					fill : "#d8ae64"
-				}
-			},
-			defaultPlot : {
-				type : "circle",
-				href : "#",
-				attrs: {
-					fill : "#f95a61"
-				}
-			},
-			eventHandlers: {
-				click : function (){
-					this.href="locations/<%= slug %>"  
-				}
-			}
-		},
-		plots: {
-			'nc' : {
-				latitude: 35.7806,
-				longitude: -78.6389,
-				tooltip: {content : "Raleigh-Durham"}
-			},	
-		}
-	});
+ 	
 });
 
