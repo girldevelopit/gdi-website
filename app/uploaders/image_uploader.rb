4068ff6d2b8847badb30680a4ba3e57503683dc1
Chet Corey <chetcorey@hotmail.com>
2014-08-16 11:11:33 -0400

    image resizing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,26 +1,16 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  # process :store_dimensions
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process :resize_to_fit => [300, nil]
-  process :crop => '300x300+0+0'
+  process :resize_to_fill => [300, 300]
 
   version :thumb do
     process :resize_to_fit => [100, 150]
-  end
-
-  version :profile do
-    process :resize_to_fit => [300, nil]
-    process :crop => '200x200+0+0'
   end
 
   def extension_white_list
@@ -33,20 +23,5 @@ class ImageUploader < CarrierWave::Uploader::Base
            else "200x200"
            end
     "holder.js/#{size}/text:#{size}/social"
-  end
-
-  private
-
-  # def store_dimensions
-  #   if file && model
-  #     model.width, model.height = ::MiniMagick::Image.open(file.file)[:dimensions]
-  #   end
-  # end
-
-  def crop(geometry)
-  manipulate! do |img|
-    img.crop(geometry)
-    img
-    end
   end
 end
