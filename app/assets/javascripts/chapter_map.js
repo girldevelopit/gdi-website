03ee1e66ef71787da18407e00cc5036a885c7513
Ruthan <rcfreese@gmail.com>
2014-08-16 21:26:48 -0400

    re-nest FOR GREAT JUSTICE

--- app/assets/javascripts/chapter_map.js
+++ app/assets/javascripts/chapter_map.js
@@ -1,53 +1,55 @@
-var locationsURL = "http://localhost:3000/locations.json"
+var locationsURL = "http://localhost:3000/locations.json";
 
 $(document).ready(function(){
-	
-	$.getJSON(locationsURL).done(function (chapter) { 
-		console.log(chapter);
-		_.each(chapter, function (plot) {
-			console.log(plot);
+	var plots = []
 
-			$(".us_map").mapael({
-				map : {
-					name : "usa_states",
-					cssClass : "map",
-					tooltip : {
-						cssClass : "mapTooltip" //class name of the tooltip container
-						},
-					defaultArea : {
-						attrs : {
-							fill : "#282828",
-							stroke: "#9a9a9a",
-						},
-						attrsHover : {
-							fill : "#d8ae64"
-						}
-					},
-					defaultPlot : {
-						type : "circle",
-						href : "#",
-						attrs: {
-							fill : "#f95a61"
-						}
-					},
-					eventHandlers: {
-						click : function (){
-							this.href="locations/<%= slug %>"  
-						}
-					}
-				},
-				plots: {},	
-			});
-		});
-		var newPlots = {
-			"<%= location %>" : {
-			latitude: "<%= latitude %>",
-			longitude: "<%= longitude %>",
-			//href: "locations/<% slug %>",
-			//tooltip: {content : "<% location %>"}
+	$.getJSON(locationsURL).done(function (chapters) {
+		// 	console.log(chapters);
+			// _.each(chapter, function (plot) {
+			// 	console.log(plot);
+			// });
+		_.each(chapters, function (plot) {
+			newobj = {
+				value: plot.location,
+				latitude: plot.latitude,
+				longitude: plot.longitude
 			}
-		};
-		$(".us_map").trigger("update", newPlots);
-	});
-});
+			plots.push(newobj);
+		})
+		console.log("yey!");
+	}).done(function () {
+		$(".us_map").mapael({
 
+		map : {
+			name : "usa_states",
+			cssClass : "map",
+			tooltip : {
+				cssClass : "mapTooltip" //class name of the tooltip container
+				},
+			defaultArea : {
+				attrs : {
+					fill : "#282828",
+					stroke: "#9a9a9a",
+				},
+				attrsHover : {
+					fill : "#d8ae64"
+				}
+			},
+			defaultPlot : {
+				type : "circle",
+				href : "#",
+				attrs: {
+					fill : "#f95a61"
+				}
+			},
+			eventHandlers: {
+				click : function (){
+					this.href="locations/<%#= @locations.location %>" // needs to be something else
+				}
+			}
+		},
+
+		plots : plots
+		})
+	})
+});
