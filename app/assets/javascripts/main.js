4813410a3b837ea0bca0bfa491094755deaade13
amygori <amydgori@gmail.com>
2014-08-18 14:16:58 -0400

    add jQuery accordion to mobile home page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,1 +1,12 @@
+//Accordion for mobile chapter index
+(function($) {
+    
+  var allPanels = $('.accordion > dd').hide();
+    
+  $('.accordion > dt > a').click(function() {
+    allPanels.slideUp();
+    $(this).parent().next().slideDown();
+    return false;
+  });
 
+})(jQuery);
