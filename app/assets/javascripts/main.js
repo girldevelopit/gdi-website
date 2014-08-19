d0c90dcd4c8356df5dfc4ffe3f661489eb945781
Ruthan <rcfreese@gmail.com>
2014-08-19 00:31:48 -0400

    Bogus commit

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,9 @@
-//Accordion for mobile chapter index
-$(function() {
-  $('#state-list .accordion').on('click', 'h2', function() {
-      $(this).parent().removeClass("hide-chapters").siblings().addClass("hide-chapters");
-  });
-});
+// //Accordion for mobile chapter index
+// $(function() {
+//   $('#state-list .accordion').on('click', 'h2', function() {
+//     $(this).children().toggleClass("red blue")
+//       // $(this).children().removeClass("hide-chapters").siblings().children().addClass("hide-chapters");
+//   });
+// });
 
-
-      
-
-
+$(".hide-chapters").accordion();
