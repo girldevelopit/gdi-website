a8d1272b1708362ddb138b2709d7a4782b547bd2
Chet Corey <chetcorey@hotmail.com>
2014-08-21 14:52:39 -0400

    fixing locations page

--- app/admin/location.rb
+++ app/admin/location.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Location do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :location, :blurb, :fb, :meetup, :twitter, :linkedin, :github, :geo
+  permit_params :location, :blurb, :fb, :meetup, :twitter, :linkedin, :github, :geo, :email, :meetup_id
   #
   index do
     selectable_column
@@ -28,9 +28,11 @@ ActiveAdmin.register Location do
   form do |f|
     f.inputs "Edit Location" do
       f.input :location, placeholder: "Los Angeles"
-      f.input :geo, placeholder: "Los Angeles, CA, USA"
-      f.input :fb, placeholder: "GDILosAngeles"
+      f.input :geo, label: "Address", placeholder: "Los Angeles, CA, USA"
+      f.input :fb, label: "Facebook", placeholder: "GDILosAngeles"
       f.input :meetup, placeholder: "Girl-Develop-It-Los-Angeles"
+      f.input :meetup_id, label: "Meetup ID", placeholder: "12345678"
+      f.input :email, placeholder: "firstname@girldevelopit.com"
       f.input :twitter, placeholder: "GDILosAngeles"
       f.input :linkedin, placeholder: "https://www.linkedin.com/groups/Girl-Develop-It-Los-Angeles-9999999"
       f.input :github, placeholder: "GDILosAngeles"
