7a6936a8c1083209886e2a362b086b43873d02ad
Chet Corey <chetcorey@hotmail.com>
2014-08-22 14:11:38 -0400

    cleaning up home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,7 @@ class HomeController < ApplicationController
   @locations = Location.all
   @location = Location.all
 
-#
-#     params = { category: '1',
-#       zip: '27701',
-#       format: 'json',
-#       page: '5'}
-#     meetup_api = MeetupApi.new
-#     @events = meetup_api.open_events(params) ## Currently events just gets
-  end    ## dumped straight into the page. It is ugly, but there is information,
-         ## and we can get better information out of it :)
+  end
 
   def about
   end
