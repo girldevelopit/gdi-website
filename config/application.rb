01f0a80e381c93e345e5c2726cad4de075e1b37b
Ruthan Freese <ruthan@rfreese.local>
2014-11-22 15:33:31 -0500

    Add favicon to precompile list

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.precompile += [
+    'favicon.ico'
+]
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
