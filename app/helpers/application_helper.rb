72922c2fa903aa8db76f1b4a2ccf57fc51b39ab7
Brandon Mikulka <brandon.mikulka@colorado.edu>
2015-01-31 14:31:13 -0700

    Add dynamic title to all pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
   def active_page(path)
     "active-nav" if current_page?(path)
   end
+
+  def full_title(page_title = '')
+    base_title = "Girl Develop It"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title}  -  #{base_title}"
+    end
+  end
 end
