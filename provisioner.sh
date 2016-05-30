5c4fe5d1b2ea8b36f69c829484b90b379e103f8b
amygori <amydgori@gmail.com>
2016-05-30 18:45:47 -0400

    Specify bash shell for Vagrant environment

--- provisioner.sh
+++ provisioner.sh
@@ -1,3 +1,6 @@
+# the following line specifies what shell to use to execute the script; it is not a comment!
+#!/usr/bin/env bash
+
 echo "Your development environment is being configured!"
 echo "Installing ruby and postgresql..."
 
@@ -5,11 +8,11 @@ echo "Installing ruby and postgresql..."
 
 apt-get update -y >>/tmp/provision-script.log 2>&1
 
-#install ruby
+# install ruby
 apt-get install ruby2.1.7 -y >>/tmp/provision-script.log 2>&1
 
-#install postgres
+# install postgres
 apt-get install postgresql postgresql-contrib -y >>/tmp/provision-script.log 2>&1
 
-#install bundler
+# install bundler
 gem install bundler
