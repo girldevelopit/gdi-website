708b40ee63153ab16301dad66bebe1d5270656e0
amygori <amydgori@gmail.com>
2016-06-05 23:07:05 -0400

    Vgrantfile and shell script provisioner updates

--- Vagrantfile
+++ Vagrantfile
@@ -22,7 +22,7 @@ Vagrant.configure(2) do |config|
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
-  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 80, host: 8080, auto_correct: true
 
   # Create a private network, which allows host-only access to the machine
   # using a specific IP.
@@ -37,19 +37,19 @@ Vagrant.configure(2) do |config|
   # the path on the host to the actual folder. The second argument is
   # the path on the guest to mount the folder. And the optional third
   # argument is a set of non-required options.
-  # config.vm.synced_folder "../data", "/vagrant_data"
+  config.vm.synced_folder ".", "/opt/gdi/development"
 
   # Provider-specific configuration so you can fine-tune various
   # backing providers for Vagrant. These expose provider-specific options.
   # Example for VirtualBox:
   #
-  # config.vm.provider "virtualbox" do |vb|
-  #   # Display the VirtualBox GUI when booting the machine
-  #   vb.gui = true
-  #
-  #   # Customize the amount of memory on the VM:
-  #   vb.memory = "1024"
-  # end
+  config.vm.provider "virtualbox" do |vb|
+    # Display the VirtualBox GUI when booting the machine
+    #vb.gui = true
+
+    # Customize the amount of memory on the VM:
+    vb.memory = "1024"
+  end
   #
   # View the documentation for the provider you are using for more
   # information on available options.
@@ -64,7 +64,21 @@ Vagrant.configure(2) do |config|
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
-  config.vm.provision "shell" do |s|
-    s.path = 'provisioner.sh'
-  end
+    #config.vm.provision :shell, path: "install-rvm.sh", args: "stable", privileged: false
+    #config.vm.provision :shell, path: "install-ruby.sh", args: "2.1.7", privileged: false
+    #config.vm.provision :shell, path: 'provisioner.sh'
+
+    # Ruby dev box by forgecrafted
+  # https://github.com/forgecrafted/vagrant-provision-ruby
+  # config.vm.provision "shell", inline: <<-SCRIPT
+  #   # Define user settings for rbenv installation.
+  #   # Current default values (shown here) are normally fine.
+  #   export USER_NAME=vagrant
+  #   export USER_HOME=/home/$USER_NAME
+  #   export DEFAULT_RUBY='2.1.7'
+
+  #   curl -fsS https://raw.githubusercontent.com/forgecrafted/vagrant-provision-ruby/master/script | bash
+  # SCRIPT
+
+  config.vm.provision "shell", path: 'provisioner.sh'
 end
--- provisioner.sh
+++ provisioner.sh
@@ -1,21 +1,71 @@
-# the following line specifies what shell to use to execute the script; it is not a comment!
-#!/usr/bin/env bash
+#!/bin/bash
 
-echo "Your development environment is being configured!"
-echo "Installing ruby and postgresql..."
+apt-get -y update
+apt-get -yfV dist-upgrade
 
-#append '>>/tmp/provision-script.log 2>&1' to log output to file
+# Install dependencies.
+echo "Installing Git & other dependencies..."
+apt-get install -yfV         \
+  build-essential            \
+  curl                       \
+  git-core                   \
+  libcurl4-openssl-dev       \
+  libreadline-dev            \
+  libssl-dev                 \
+  libxml2-dev                \
+  libxslt1-dev               \
+  libyaml-dev                \
+  python-software-properties \
+  zlib1g-dev                 \
+  libffi-dev                 \
+  libpq-dev                  \
+  imagemagick                \
+  libmagickwand-dev          \
+  nginx                      \
 
-apt-get update -y >>/tmp/provision-script.log 2>&1
+# get and compile Ruby
+echo "Getting & compiling Ruby...This will take a few minutes."
+curl -LSso /tmp/ruby-2.1.7.tar.gz ftp://ftp.ruby-lang.org/pub/ruby/2.1/ruby-2.1.7.tar.gz
+tar -zxf /tmp/ruby-2.1.7.tar.gz -C /tmp
 
-# install ruby
-apt-get install ruby2.1.7 -y >>/tmp/provision-script.log 2>&1
+cd /tmp/ruby-2.1.7 && \
+  ./configure && \
+  make && \
+  sudo make install > /dev/null
+
+# install nodejs
+echo "Installing NodeJS"
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get -y update
+apt-get install -y nodejs
 
 # install postgres
-apt-get install postgresql-9.3 postgresql-contrib -y >>/tmp/provision-script.log 2>&1
+echo "Installing PostgreSQL"
+set -x
+echo "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main 9.3" \
+    | sudo tee -a /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get -y update
+sudo apt-get -y install postgresql-9.3
+sudo apt-get -y install postgresql-contrib-9.3
+sudo su -c 'createuser -s vagrant with password "vagrant"'  postgres
 
-# install other project dependencies
-apt-get install build-essential libpq-dev  imagemagick libmagickwand-dev nodejs
+sudo sed -ibk \
+    -re "s/[#]*listen_addresses = '.*'(.*)/listen_addresses = '*'\1/" \
+    /etc/postgresql/9.3/main/postgresql.conf
+echo "host    all   vagrant   10.0.2.2/32  trust" | sudo tee -a /etc/postgresql/9.3/main/pg_hba.conf
+sudo service postgresql restart
 
-# install bundler
+
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
+echo 'installing Bundler'
 gem install bundler
+
+gem uninstall pg -3
+# Remove any leftovers
+apt-get -y autoremove
+
+sudo service nginx restart
+
+cd ../opt/gdi/development
+bundle install
