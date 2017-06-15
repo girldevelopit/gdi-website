7b84bc22ba078c29bef64496356f980a26c064b4
Ben Tilkens <tilke002@gmail.com>
2017-06-15 02:14:32 +0000

    Updating 2017 Leadership Summit page and adding Gems needed for feature specs

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ group :development do
 end
 
 group :test do
+  gem 'capybara'
   gem "rspec"
   gem 'rspec-rails'
   gem 'simplecov-rcov'
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,5 @@
+GOOGLE_ANALYTICS_TRACKING_CODE = ''
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
