4edbf71559a10c3676c544cf82947321cec372f8
= <=>
2017-07-26 00:28:58 -0400

    fixing carrierwave order to restore deployment

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,12 +3,12 @@ CarrierWave.configure do |config|
   config.directory_permissions = 0777
 
   if Rails.env.production?
-    config.storage = :fog
     config.fog_credentials = {
         :provider               => 'AWS',
         :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
         :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
     }
+    config.storage = :fog
     config.fog_directory = ENV['S3_BUCKET_NAME']
   else
     config.storage = :file
