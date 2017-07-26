dbd8d35488a1f8503daa63798e845436db944dfa
= <=>
2017-07-26 01:07:53 -0400

    more config adjustments to get fog aws to work

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,19 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
   config.permissions = 0666
   config.directory_permissions = 0777
 
   if Rails.env.production?
+    config.fog_provider = 'fog/aws'
+    config.storage = :fog
     config.fog_credentials = {
         :provider               => 'AWS',
         :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
         :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
     }
-    config.storage = :fog
     config.fog_directory = ENV['S3_BUCKET_NAME']
   else
     config.storage = :file
