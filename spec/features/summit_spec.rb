04a8f1a052147404ef43c208ad8b31ba6f0c307d
LeeAnn <leeann.a.kinney@gmail.com>
2018-08-11 16:04:08 -0400

    updates summit_spec.rb - hoping this fixes travis build fail

--- spec/features/summit_spec.rb
+++ spec/features/summit_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe 'summit page' do
   context 'visit the summit page' do
-    it 'displays 2017 summit content' do
+    it 'displays 2018 summit content' do
       visit '/summit'
-      expect(page).to have_content('2017 Leadership Summit')
+      expect(page).to have_content('2018 Leadership Summit')
     end
   end
 end
